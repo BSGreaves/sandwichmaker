7d96263ead0c610351c7f48e50a7aba1cfea3819
Ben Greaves <bsgreaves@gmail.com>
2017-02-18 20:10:31 -0600

    More js structure

--- js/main.js
+++ js/main.js
@@ -1,23 +1,7 @@
 var button = document.getElementById("button");
 var checkBoxes = document.querySelectorAll("[id^=opt]");
-var testClass = document.getElementsByClassName("test");
 var selectedAddons = [];
 
-
-
-// var test;
-// console.log(allIds);
-
-// // allIds.forEach(function(each, key){
-// // 	console.log(key);
-// // 	console.log(each);
-// // 	var test = key;
-
-// // });
-
-// for (var value of allIds.value){
-// 	console.log(value);
-// };
 
 button.addEventListener("click", function(){
 	determineSelectedAddons(checkBoxes);
@@ -32,3 +16,4 @@ function determineSelectedAddons (nodeList) {
 	console.log(selectedAddons);
 };
 
+// function calculatePrice ()
